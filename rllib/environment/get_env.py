6487f997a6502eadf97330d3e8dfcdb4df883458
nuriaarmengol <nuria_a_u@msn.com>
2020-11-15 12:05:59 +0100

    Updating get_env to create datasets directly

--- rllib/environment/get_env.py
+++ rllib/environment/get_env.py
@@ -4,205 +4,100 @@ import d4rl
 import gym
 import h5py
 from rllib.dataset.experience_replay import D4RL_Dataset
+from rllib.environment.reward_wrappers import (RewardHighVelocity,
+                                               RewardUnhealthyPose)
+from rllib.util.create_hdf5 import HDF5_Creator
 from utils.utilities import get_keys
 
-from .reward_wrappers import RewardHighVelocity, RewardUnhealthyPose
+path_to_datasets = os.environ.get('D4RL_DATASET_DIR',
+                                  os.path.expanduser('~/.d4rl/datasets'))
 
 
-def get_env(p, SEED=None, eval=False, noise=False, sigma_noise=None,
+def get_gym_name(dataset_name):
+    # Get v3 version of environments for extra information to be available
+    if 'cheetah' in dataset_name:
+        return 'HalfCheetah-v3'
+    elif 'hopper' in dataset_name:
+        return 'Hopper-v3'
+    elif 'walker' in dataset_name:
+        return 'Walker2d-v3'
+    else:
+        raise ValueError("{dataset_name} is not in D4RL")
+
+
+def get_env(p, SEED=None, eval=False, noise=False,
             eval_terminate_when_unhealthy=True):
+
     if 'O_RAAC' in p.agent.name:
-        path = os.environ.get('D4RL_DATASET_DIR',
-                              os.path.expanduser('~/.d4rl/datasets'))
 
         terminate_when_unhealthy = False \
             if not p.env.terminate_when_unhealthy \
             or not eval_terminate_when_unhealthy else True
 
-        # Expertise level of Behavior policy:
-        expertise_level = None
-        expertise_levels = ['expert', 'medium']
-        for level in expertise_levels:
-            if level in p.env.name:
-                expertise_level = level
-        assert expertise_level is not None, 'No expertise level found in \
-                                            datasets'
+        env_d4rl = gym.make(p.env.name)
+        dataset_name = env_d4rl.dataset_filepath[:-5]
+        # Use v3 version of environments for extra information to be available
+        kwargs = {'terminate_when_unhealthy': terminate_when_unhealthy} if \
+            'cheetah' not in dataset_name else {}
+        env = gym.make(get_gym_name(dataset_name),
+                       **kwargs
+                       ).unwrapped
+        if SEED is None:
+            env.unwrapped.seed(seed=p.agent.SEED)
+        else:
+            env.unwrapped.seed(seed=SEED)
 
-        if 'cheetah' in p.env.name:
-            # use normalEnv to be able to use extra info
-            env = gym.make('HalfCheetah-v3').unwrapped
-            
-            if SEED is None:
-                env.unwrapped.seed(seed=p.agent.SEED)
-            else:
-                env.unwrapped.seed(seed=SEED)
+        if p.env.prob_vel_penal is not None and p.env.prob_vel_penal > 0:
+            dict_env = {'prob_vel_penal': p.env.prob_vel_penal,
+                        'cost_vel': p.env.cost_vel,
+                        'max_vel': p.env.max_vel}
 
-            if p.env.prob_vel_penal is not None and p.env.prob_vel_penal > 0:
-                dict_env = {'prob_vel_penal': p.env.prob_vel_penal,
-                            'cost_vel': p.env.cost_vel,
-                            'max_vel': p.env.max_vel}
-                # Get dataset:
-                fname = f'halfcheetah_{expertise_level}_'\
-                    f'redone_prob{dict_env["prob_vel_penal"]}'\
-                    f'_penal{dict_env["cost_vel"]}_'\
-                    f'maxvel{dict_env["max_vel"]}.hdf5'
+            fname = f'{dataset_name}_'\
+                f'prob{dict_env["prob_vel_penal"]}_'\
+                f'penal{dict_env["cost_vel"]}_'\
+                f'maxvel{dict_env["max_vel"]}.hdf5'
 
-                env = RewardHighVelocity(env, **dict_env)
-            else:
-                fname = f'halfcheetah_{expertise_level}_redone.hdf5'
+            env = RewardHighVelocity(env, **dict_env)
 
-            if not eval:
-                h5path = os.path.join(
-                    path, fname)
-                dataset_file = h5py.File(h5path, 'r')
-                data = {k: dataset_file[k][:] for k in get_keys(dataset_file)}
-                dataset_file.close()
-                print(f'Checking dataset {h5path} is correct')
-                # Use get_dataset method from OfflineEnv Class in
-                # D4RL to check dataset
-                env_test = gym.make(p.env.name)
-                env_test.get_dataset(h5path=h5path)
+        elif p.env.prob_pose_penal is not None and \
+                p.env.prob_pose_penal > 0:
+            dict_env = {'prob_pose_penal': p.env.prob_pose_penal,
+                        'cost_pose': p.env.cost_pose,
+                        }
 
-        elif 'walker' in p.env.name:
-
-            # 'use normalEnv to be able to use extra info
-            # env = gym.make('Walker2d-v3', reset_noise_scale = 0).unwrapped
-            env = gym.make(
-                'Walker2d-v3',
-                terminate_when_unhealthy=terminate_when_unhealthy).unwrapped
-            if SEED is None:
-                env.unwrapped.seed(seed=p.agent.SEED)
-            else:
-                env.unwrapped.seed(seed=SEED)
-
-            if p.env.prob_vel_penal is not None and p.env.prob_vel_penal > 0:
-                dict_env = {'prob_vel_penal': p.env.prob_vel_penal,
-                            'cost_vel': p.env.cost_vel,
-                            'max_vel': p.env.max_vel}
-
-                if terminate_when_unhealthy:
-                    fname = f'walker2d_{expertise_level}_'\
-                        f'redone_prob{dict_env["prob_vel_penal"]}'\
-                        f'_penal{dict_env["cost_vel"]}_'\
-                        f'maxvel{dict_env["max_vel"]}.hdf5'
-                else:
-                    fname = f'walker2d_{expertise_level}_'\
-                        f'redone_prob{dict_env["prob_vel_penal"]}'\
-                        f'_penal{dict_env["cost_vel"]}_'\
-                        f'maxvel{dict_env["max_vel"]}_noterminate.hdf5'
-
-                env = RewardHighVelocity(env, **dict_env)
-
-            elif p.env.prob_unhealthy_penal is not None and \
-                    p.env.prob_unhealthy_penal > 0:
-                dict_env = {'prob_unhealthy_penal': p.env.prob_unhealthy_penal,
-                            'cost_pose': p.env.cost_pose,
-                            }
-
-                if terminate_when_unhealthy:
-                    fname = f'walker2d_{expertise_level}_'\
-                        f'redone_prob{dict_env["prob_unhealthy_penal"]}'\
-                        f'_penal{dict_env["cost_pose"]}_'\
-                        f'unhealthy_position.hdf5'
-                else:
-                    fname = f'walker2d_{expertise_level}_'\
-                        f'redone_prob{dict_env["prob_unhealthy_penal"]}'\
-                        f'_penal{dict_env["cost_pose"]}_'\
-                        'unhealthy_position_noterminate.hdf5'
-
-                env = RewardUnhealthyPose(env, **dict_env)
-
-            else:
-                fname = f'walker2d_{expertise_level}_redone.hdf5'
-
-            if not eval:
-
-                h5path = os.path.join(
-                    path, fname)
-                dataset_file = h5py.File(h5path, 'r')
-                data = {k: dataset_file[k][:] for k in get_keys(dataset_file)}
-                dataset_file.close()
-                print(f'Checking dataset {h5path} is correct')
-                # Use get_dataset method from OfflineEnv Class in D4RL to
-                # check dataset
-                env_test = gym.make(p.env.name)
-                env_test.get_dataset(h5path=h5path)
-
-        elif 'hopper' in p.env.name:
-
-            # use normalEnv to be able to use extra info
-            env = gym.make(
-                'Hopper-v3',
-                terminate_when_unhealthy=terminate_when_unhealthy).unwrapped
-            # env = gym.make('Hopper-v3', reset_noise_scale=0).unwrapped
-            if SEED is None:
-                env.unwrapped.seed(seed=p.agent.SEED)
-            else:
-                env.unwrapped.seed(seed=SEED)
-
-            if p.env.prob_vel_penal is not None and p.env.prob_vel_penal > 0:
-                dict_env = {'prob_vel_penal': p.env.prob_vel_penal,
-                            'cost_vel': p.env.cost_vel,
-                            'max_vel': p.env.max_vel}
-
-                if terminate_when_unhealthy:
-                    fname = f'hopper_{expertise_level}_'\
-                        f'redone_prob{dict_env["prob_vel_penal"]}'\
-                        f'_penal{dict_env["cost_vel"]}_'\
-                        f'maxvel{dict_env["max_vel"]}.hdf5'
-                else:
-                    fname = f'hopper_{expertise_level}_'\
-                        f'redone_prob{dict_env["prob_vel_penal"]}'\
-                        f'_penal{dict_env["cost_vel"]}_'\
-                        f'maxvel{dict_env["max_vel"]}_noterminate.hdf5'
-
-                # use 'normal' env for
-                env = RewardHighVelocity(env, **dict_env)
-                # reproducibility (offlineEnv is not reproducible)
-
-            elif p.env.prob_unhealthy_penal is not None and \
-                    p.env.prob_unhealthy_penal > 0:
-                dict_env = {'prob_unhealthy_penal': p.env.prob_unhealthy_penal,
-                            'cost_pose': p.env.cost_pose,
-                            }
-
-                if terminate_when_unhealthy:
-                    fname = f'hopper_{expertise_level}_'\
-                        f'redone_prob{dict_env["prob_unhealthy_penal"]}'\
-                        f'_penal{dict_env["cost_pose"]}_'\
-                        f'unhealthy_position.hdf5'
-                else:
-                    fname = f'hopper_{expertise_level}_'\
-                        f'redone_prob{dict_env["prob_unhealthy_penal"]}'\
-                        f'_penal{dict_env["cost_pose"]}_'\
-                        'unhealthy_position_noterminate.hdf5'
-
-                env = RewardUnhealthyPose(env, **dict_env)
-
-            else:
-                fname = f'hopper_{expertise_level}_redone.hdf5'
-
-            if not eval:
-                h5path = os.path.join(
-                    path, fname)
-                dataset_file = h5py.File(h5path, 'r')
-                data = {k: dataset_file[k][:] for k in get_keys(dataset_file)}
-                dataset_file.close()
-                print(f'Checking dataset {h5path} is correct')
-
-                # Use get_dataset method from OfflineEnv Class in D4RL to
-                # check dataset
-                env_test = gym.make(p.env.name)
-                env_test.get_dataset(h5path=h5path)
+            fname = f'{dataset_name}_'\
+                    f'prob{dict_env["prob_pose_penal"]}_'\
+                    f'penal{dict_env["cost_pose"]}_'\
+                    'pose.hdf5'
+            env = RewardUnhealthyPose(env, **dict_env)
 
         else:
-            raise ValueError(f'Environment {p.env.name} is not valid')
+            fname = env_d4rl.dataset_filepath
 
-        # Prepare dataset for Offline Training:
+        # Get dataset for training:
         if not eval:
+            h5py_path = os.path.join(path_to_datasets, fname)
+            if not os.path.exists(h5py_path):
+                creator = HDF5_Creator(d4rl_env_name=p.env.name,
+                                       properties_env=dict_env,
+                                       fname=fname)
+                creator.create_hdf5_file()
+            dataset_file = h5py.File(h5py_path, 'r')
+            data = {k: dataset_file[k][:] for k in get_keys(dataset_file)}
+            dataset_file.close()
+
+            print(f'\nChecking dataset {h5py_path} is correct...')
+            # Use get_dataset method from OfflineEnv Class in
+            # D4RL to check dataset
+            env_d4rl.get_dataset(h5path=h5py_path)
+            print('Dataset correct\n')
+
             dataset = D4RL_Dataset(data)
         if eval:
             dataset = None
 
-        print(env, dataset)
         return env, dataset
+
+    else:
+        raise ValueError(f'Only O-RAAC agent name is compatible for the '
+                         f'moment. {p.agent.name} was given')
---
+++ rllib/util/create_hdf5.py
@@ -0,0 +1,136 @@
+import os
+
+import gym
+import numpy as np
+import d4rl  # Import required to register environments
+from rllib.dataset.utilities import DatasetWriter
+from rllib.environment.reward_wrappers import (RewardHighVelocity,
+                                               RewardUnhealthyPose)
+import torch
+
+path_to_datasets = os.environ.get('D4RL_DATASET_DIR',
+                                  os.path.expanduser('~/.d4rl/datasets'))
+
+
+def get_gym_name(dataset_name):
+    if 'cheetah' in dataset_name:
+        return 'HalfCheetah-v3'
+    elif 'hopper' in dataset_name:
+        return 'Hopper-v3'
+    elif 'walker' in dataset_name:
+        return 'Walker2d-v3'
+    else:
+        raise ValueError("{dataset_name} is not in D4RL")
+
+
+class HDF5_Creator():
+
+    """Create a hdf5 file containing training data for offline RL algorithm
+    with a new reward function.
+    Original dataset is obtained from D4RL environemnt but new data
+    is obtained by running version 3 of the same environment type to get
+    additional information only provided in version 3.
+
+    Parameters
+    ----------
+    d4rl_env_name: str
+                Name of the original hdf5 provided by D4RL.
+                Example: 'walker2d-expert-v0'
+    properties_env: dict
+            dictionary containing information about the new reward function
+
+            For speed penalization:
+            dict_env = {'prob_vel_penal': float,
+                            'cost_vel': int,
+                            'max_vel': float}
+            For pose penalization:
+            dict_env = {'prob_pose_penal':float,
+                            'cost_pose': int,
+                            }
+    """
+
+    def __init__(self, d4rl_env_name, properties_env=None, fname=None):
+        self.env_d4rl = gym.make(d4rl_env_name)
+        dataset = self.env_d4rl.get_dataset()
+        self.writer = DatasetWriter(mujoco=True)
+
+        env = gym.make(get_gym_name(d4rl_env_name)).unwrapped
+        env.seed(10)
+        torch.manual_seed(10)
+        np.random.seed(10)
+        self.actions = dataset['actions']
+        self.obs = dataset['observations']
+        self.rewards = dataset['rewards']
+        self.dones = dataset['terminals']
+        self.properties_env = properties_env
+
+        if properties_env.get('cost_vel', False):
+            self.env = RewardHighVelocity(env, **properties_env)
+        elif properties_env.get('cost_pose', False):
+            self.env = RewardUnhealthyPose(env, **properties_env)
+        else:
+            raise ValueError('No reward wrapper found')
+
+        dataset_name = self.env_d4rl.dataset_filepath[:-5]
+
+        self.h5py_name = f'{dataset_name}_'\
+            f'prob{properties_env["prob_vel_penal"]}_'\
+            f'penal{properties_env["cost_vel"]}_'\
+            f'maxvel{properties_env["max_vel"]}.hdf5'
+
+        assert fname == self.h5py_name, \
+            f'Not same name for h5py file {fname} vs {self.h5py_name}'
+
+    def get_state(self, i):
+        pos_full = np.concatenate([[self.env.sim.data.qpos[0].copy()],
+                                   self.obs[i][0:8]]
+                                  )  # pos_full = [xpos, 8jointpos]
+        vel_full = self.obs[i][8::]  # [9 jointvels]
+        return pos_full, vel_full
+
+    def create_hdf5_file(self):
+        self.writer._reset_data()
+        init_pos = np.concatenate([[0], self.obs[0][0:8]])
+        init_vel = self.obs[0][8::]
+
+        self.env.reset()
+        print(len(init_pos), (len(init_vel)))  # check
+        self.env.set_state(qpos=init_pos, qvel=init_vel)
+
+        print('\n\n **** Creating new dataset...******\n\n')
+        for i in range(len(self.actions)):
+            pos_full, vel = self.get_state(i)  # contains xposition
+            observation = np.concatenate(
+                (pos_full[1:], vel)).ravel()  # remove xposition
+            action = self.actions[i]
+            if not i % 10000:
+                print(f'Num datapoint {i}/{len(self.actions)}')
+
+            # reset state to data, needed to run open-loop
+            self.env.set_state(qpos=pos_full, qvel=vel)
+            _, reward, _, info = self.env.step(self.actions[i])
+
+            if self.properties_env is None:  # no probpenal
+                r = self.rewards[i]
+            else:
+                r = reward
+            self.writer.append_data(observation, action, r, self.dones[i],
+                                    mujoco_env_data=[
+                                    pos_full, vel], info=info)
+
+        self.writer.write_dataset(fname=self.h5py_name)
+        print(f'Checking dataset {self.h5py_name} is correct...')
+        # Use get_dataset method from OfflineEnv Class in
+        # D4RL to check dataset
+        new_dataset = self.env_d4rl.get_dataset(h5path=self.h5py_name)
+
+        for _ in range(10):
+            random_datapoint = np.random.randint(0, len(self.dataset))
+            assert all(self.dataset['actions'][random_datapoint] ==
+                       new_dataset['actions'][random_datapoint]),\
+                f'{self.h5py_name} is not correct. Not same actions!'
+            assert all(self.dataset['observations'][random_datapoint] ==
+                       new_dataset['observations'][random_datapoint]),\
+                f'{self.h5py_name} is not correct. Not same observations!'
+
+        print(f'Dataset correct. Dataset saved in {self.h5py_name}')
